b521addc1206c4bf6727936279683e642945e7f2
Chris <nzeanoruec@gmail.com>
2024-02-23 17:37:38 -0500

    Fixed margin-top of scroll btn

--- src/components/scrollButton/ScrollBtnStyle.ts
+++ src/components/scrollButton/ScrollBtnStyle.ts
@@ -7,7 +7,7 @@ const BtnScroll = styled.div`
 	background-color: rgba(0, 0, 0, 0.15);
 	border-radius: 2px;
 	display: inline-block;
-	margin-top: 15px;
+	margin-top: 8px;
 
 	@media screen and (min-width: 1024px) {
 		position: fixed;
