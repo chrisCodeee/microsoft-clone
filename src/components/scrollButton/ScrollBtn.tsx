1e9b9501c7655b5082437ec1ee45dab0adb0236c
Chris <nzeanoruec@gmail.com>
2024-02-23 13:32:34 -0500

    Added margin top

--- src/components/scrollButton/ScrollBtn.tsx
+++ src/components/scrollButton/ScrollBtn.tsx
@@ -3,7 +3,7 @@ import { HiArrowSmallUp } from "react-icons/hi2";
 
 const ScrollBtn = () => {
 	return (
-		<div className="d-flex justify-content-end">
+		<div className="d-flex justify-content-end mt-5">
 			<BtnScroll>
 				<a href="#top" className="d-flex">
 					<HiArrowSmallUp className="me-3" size={20} />
