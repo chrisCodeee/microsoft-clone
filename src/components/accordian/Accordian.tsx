e5785bf43b2f236d8cc0d2b4add0ec8050b2528e
Chris <nzeanoruec@gmail.com>
2024-02-25 10:24:10 -0500

    Fixed the border bottom

--- src/components/accordian/Accordian.tsx
+++ src/components/accordian/Accordian.tsx
@@ -7,7 +7,7 @@ const Accordian = () => {
 		<Accordion flush className="py-2 ps-1">
 			<Accordion.Item eventKey="0">
 				<Accordion.Header className="pb-2" style={{ borderBottom: "1px solid rgba(204, 204, 204, 0.4)" }}>
-					<Span>Accordion</Span>
+					<span style={{ fontSize: "1.3rem" }}>Accordion</span>
 				</Accordion.Header>
 				<Accordion.Body className="ps-5">
 					<div className="pb-4" style={{ borderBottom: "1px solid rgba(204, 204, 204, 0.4)" }}>
