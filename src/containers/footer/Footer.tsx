897d7c4b78c490b4a292477141f383f15b2e8bd2
Chris <nzeanoruec@gmail.com>
2024-02-25 08:51:04 -0500

    Fixed the margin

--- src/containers/footer/Footer.tsx
+++ src/containers/footer/Footer.tsx
@@ -41,7 +41,7 @@ const Footer = () => {
 					</div>
 				</div>
 
-				<div className="d-flex flex-wrap justify-content-start justify-content-lg-end mt-3 mt-xxl-0">
+				<div className="d-flex flex-wrap justify-content-start justify-content-md-center justify-content-lg-end mt-3 mt-xxl-0">
 					{footerLinks.map(({ name, link, copy }) => (
 						<li key={name} className="me-3 me-xxl-5 pt-2 py-xxl-0">
 							<Link to={link} className="card-link">
